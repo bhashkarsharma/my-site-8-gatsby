6d8e132524e3bce6c595dcd95b410b935c910594
Bhashkar Sharma <bhashkarsharma@gmail.com>
2019-03-04 16:19:00 +0100

    Util gives a random integer now

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,4 +1,8 @@
 export class Util {
+  /**
+   * Generate hash for the string
+   * @param str input string
+   */
   static hash(str: string): number {
     let hash = 0
     for (let i of Array(str.length).keys()) {
@@ -7,19 +11,45 @@ export class Util {
     return hash
   }
 
+  /**
+   * Convert an integer to hex color
+   * @param n number to be converted
+   */
   static intToRGB(n: number): string {
     const c = (n & 0x00ffffff).toString(16).toUpperCase()
     return n ? '#000000'.substring(0, 7 - c.length) + c : 'var(--color-link)'
   }
 
+  /**
+   * Generates a hex color code for given string
+   * @param str string to compute color
+   */
   static getColorForString(str: string): string {
     return this.intToRGB(this.hash(str || ''))
   }
 
-  static getLabelColor(color: string): string {
-    if (color.length < 5) {
-      color += color.slice(1)
+  /**
+   * Decides the best text color for
+   * the given background color based on contrast
+   * @param bgColor background color
+   */
+  static getLabelColor(bgColor: string): string {
+    if (bgColor.length < 5) {
+      bgColor += bgColor.slice(1)
     }
-    return color.replace('#', '0x') > (0xffffff / 2).toString() ? 'var(--color-black)' : 'var(--color-white)'
+    return bgColor.replace('#', '0x') > (0xffffff / 2).toString() ? 'var(--color-black)' : 'var(--color-white)'
+  }
+
+  /**
+   * Returns an integer in the given range
+   * @param min lower range. If max is not provided, will be 0.
+   * @param max (optional) upper range. If not provided, min will be used.
+   */
+  static getRand(min: number, max?: number): number {
+    if (max === undefined) {
+      max = min
+      min = 0
+    }
+    return min + Math.floor(Math.random() * (max - min))
   }
 }
